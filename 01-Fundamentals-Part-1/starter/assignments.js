ba2b1a332cef964c166838a4139e0dc282be6b20
Jason Termine <130055600+JATermine@users.noreply.github.com>
2023-04-18 12:21:06 +0200

    Assignments and Coding Challenges from the Course

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,14 +1,16 @@
-// Fundamentals #1
+// Fundamentals #1 (Contains Assignment & Challenges)
 
-/* 
+
+//////////////--->Assignments<--//////////////////
+
 //Assignment 1 START Values and Variables
-//let country = "Switzerland";
-//let continent = "EU";
-let population = 7000000;
+let country = "Switzerland";
+let continent = "EU";
+let population = 8800000;
 
-//console.log(country);
-//console.log(continent);
-//console.log(population);
+console.log(country);
+console.log(continent);
+console.log(population);
 //Assignment 1 END
 
 
@@ -17,9 +19,9 @@ let isIsland = true;
 let language;
 
 console.log(typeof isIsland);
-//console.log(typeof population);
-//console.log(typeof country);
-//console.log(typeof language);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
 //Assignment 2 END
 
 //Assignment 3 Start let, const, var
@@ -46,13 +48,16 @@ let description = 'Portugal is in Europe, and its 11 million people speak portug
 //Assignment 4 END
 
 
+
+//////////////--->Challenges<--//////////////////
+
 //Coding Challenge #1 START
+
 const massMark = 95;
 const heightMark = 1.88;
 
 const massJohn = 85;
 const heightJohn = 1.76;
-
 
 const BMI_MARK = massMark / heightMark ** 2;
 const BMI_JOHN = massJohn / heightJohn ** 2;
@@ -62,40 +67,54 @@ const markHigherBMI = BMI_MARK > BMI_JOHN;
 console.log(BMI_MARK);
 console.log(BMI_JOHN);
 console.log(markHigherBMI);
+
 //Coding Challenge #1 END
 
 
 //Assignment 5 START Strings and Template Literals
+
 let descriptionNew = `${country} is in ${continent}, and its ${population} people speak ${language}`;
 
 console.log(descriptionNew);
+
 //Assignment 5 END
 
+
 //Coding Challenge #2 START -> BMI Reference #1
+
 if (BMI_MARK > BMI_JOHN) {
   console.log(`Mark's BMI (${BMI_MARK}) is lower than John's (${BMI_JOHN})`);
 } else {
   console.log(`John's BMI (${BMI_JOHN}) is lower than Mark's (${BMI_MARK})`);
 }
+
 //Coding Challenge #2 ENDE
 
+
 //Assignment 6 START Taking Decisions: if / else statements
+
 if (population >= 33000000) {
   console.log("Portugal's population is above average");
 } else {
   console.log("Portugal's population is 22 million below average");
 }
+
 //Assignment 6 ENDE
 
+
 //Assignment 7 START Type conversion and coercion
+
 4
 617
 23
 true > false;
 117 > 1143
+
 //Assignment 7 ENDE
 
+
 //Assignment 8 START Equality Operators: == vs. ===
+
 let numNeighbours = Number(prompt('How many neighbour country does your country have?'));
 
 if (numNeighbours === 1) {
@@ -105,10 +124,12 @@ if (numNeighbours === 1) {
 } else {
   console.log('No Borders');
 }
+
 // Assignment 8 ENDE
 
 
 // Coding Challenge #3 START
+
 const dolphinsScore = 96 + 108 + 89;
 const koalasScore = 88 + 91 + 110;
 
@@ -131,6 +152,7 @@ if ((averageScoreDolphins === averageScoreKoalas) && (averageScoreDolphins >= is
 } else {
   console.log("No one wins the trophy");
 }
+
 // Coding Challenge #3 ENDE
 
 
@@ -152,8 +174,6 @@ const tip3 = bill3 >= 50 && bill3 <= 300 ? bill3 * 0.15 : bill3 * 0.2;
 console.log(`The bill was ${bill3}, the tip was ${tip3} and the total is ${bill3 + tip3}`);
 
 // Coding Challenge #4 ENDE
-
-*/
 
 
 
